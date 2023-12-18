fd8b42c20d09a89862360be0a3aaf2c88ab487ea
Petrik Diak <diak@petrik.hu>
2023-12-18 12:14:52 +0100

    2. feladat

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",init);
+let lista = [];
+
+function init(){
+    document.getElementById("gomb").addEventListener("click",feldolgoz);
+}
+
+function feldolgoz() {
+    lista.push(document.getElementById("szam").value);
+}
